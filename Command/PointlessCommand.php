e70f2b537168c33d4342d7d541df507b99b1d300
Romain Pouclet <me@palleas.com>
2011-07-13 15:56:32 +0200

    Made pointless command a little bit more funny (and pointless)

--- Command/PointlessCommand.php
+++ Command/PointlessCommand.php
@@ -7,13 +7,21 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-use Palleas\SchedulerBundle\Scheduler\Frequency as Every;
+use Palleas\SchedulerBundle\Scheduler\Every;
 
 /**
 * @Every("5min") 
 */
 class PointlessCommand extends Command
 {
+    protected $quotes = array(
+        'I say no to drugs, but they don’t listen.',
+        'If aliens are looking for intelligent life?! WHY THE HECK ARE YOU SCARED?!',
+        'A day without sunshine is like, you know, night.',
+        'It’s so simple to be wise. Just think of something stupid to say and then don’t say it.',
+        'If you have noticed this notice you will have noticed that this notice is not worth noticing',
+        'Smoking kills. If you\'re killed, you\'ve lost a very important part of your life.',
+    );
 
     protected function configure()
     {
@@ -24,7 +32,33 @@ class PointlessCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln(sprintf("Hi, it's already <info>%s</info> !", date('H:i:s')));
-    }    
+        $output->writeln($this->quotes[mt_rand(0, sizeof($this->quotes) - 1)]);
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
